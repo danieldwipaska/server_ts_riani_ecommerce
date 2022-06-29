a9a7a233e86146f01c06cca4dc602eb8807b3fae
Daniel Kamasi <scientificallychristian@gmail.com>
2022-06-30 03:00:05 +0700

    API login done

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -28,8 +39,34 @@ router.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, functio
         res.status(200).json("Register Successful!");
     }
     catch (err) {
-        console.log("error saat insert ke database");
         res.status(500).json(err);
+    }
+}));
+router.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username === undefined || password === undefined) {
+        res.status(401).json("Do not leave username or password blank");
+    }
+    else {
+        try {
+            const user = yield postgres_1.postgresPool.query("SELECT user_id, username, password, is_admin, created_at, updated_at FROM list_of_users WHERE username = $1", [username]);
+            if (user.rowCount === 0) {
+                res.status(402).json("Wrong Username");
+            }
+            else {
+                if (password != user.rows[0].password) {
+                    res.status(402).json("Wrong Password");
+                }
+                else if (password === user.rows[0].password) {
+                    const _a = user.rows[0], { password } = _a, userData = __rest(_a, ["password"]);
+                    res.status(200).json(userData);
+                }
+            }
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
     }
 }));
 //# sourceMappingURL=auth.js.map
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import express, { Router, Request, Response } from "express";
+import { QueryResult } from "pg";
 import { postgresPool } from "../models/postgres";
 
 const router: Router = express.Router();
@@ -20,8 +21,41 @@ router.post("/register", async (req: Request, res: Response) => {
     );
     res.status(200).json("Register Successful!");
   } catch (err) {
-    console.log("error saat insert ke database");
+    // console.log("error saat insert ke database");
     res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req: Request, res: Response) => {
+  const username: string | undefined = req.body.username;
+  const password: string | undefined = req.body.password;
+  if (username === undefined || password === undefined) {
+    res.status(401).json("Do not leave username or password blank");
+  } else {
+    //CHECKING USER DATA
+    try {
+      const user: QueryResult = await postgresPool.query(
+        "SELECT user_id, username, password, is_admin, created_at, updated_at FROM list_of_users WHERE username = $1",
+        [username]
+      );
+
+      //IF NOT EXIST
+      if (user.rowCount === 0) {
+        res.status(402).json("Wrong Username");
+      } else {
+        //IF WRONG PASSWORD
+        if (password != user.rows[0].password) {
+          res.status(402).json("Wrong Password");
+        } else if (password === user.rows[0].password) {
+          //IF RIGHT PASSWORD
+          const { password, ...userData } = user.rows[0];
+          res.status(200).json(userData);
+        }
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 });
 
