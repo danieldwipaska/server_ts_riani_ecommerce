eaffd5ca8537b0247ecb448bc370a06542d5432e
Daniel Kamasi <scientificallychristian@gmail.com>
2022-07-03 17:01:24 +0700

    membuat test dengan jest, tapi mengabaikan warning dari jest

--- test/test.test.js
+++ test/test.test.js
@@ -1,10 +1,28 @@
-import supertest from "supertest";
+import request from "supertest";
 import { app } from "../dist/app.js";
-const request = supertest(app);
 
-describe("POST users", () => {
-  it("Should have status 200", async () => {
-    const response = await request.get("/api/users");
-    expect(response.status).toBe(200);
+describe("GET /api/users", () => {
+  it("Should have status code 200", async () => {
+    const data = await request(app).get("/api/users");
+    expect(data.statusCode).toBe(200);
   });
+});
+
+describe("GET /api/users/:id", () => {
+  it("Should have status code 200", async () => {
+    const data = await request(app).get("/api/users/2");
+    expect(data.statusCode).toBe(200);
+  });
+  it("Should have status code 401", async () => {
+    const data = await request(app).get("/api/users/3");
+    expect(data.statusCode).toBe(401);
+  });
+});
+
+describe("", () => {
+  it("", async () => {});
+});
+
+describe("", () => {
+  it("", async () => {});
 });
